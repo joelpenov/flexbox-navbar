9b0cc19068d086c95461617548ab72b7cb2765bf
Joel Peña Arias <jukey09@gmail.com>
2016-12-29 16:18:23 -0400

    Add navbar using flexbox

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 var gulp = require("gulp");
 var prefixer = require("gulp-autoprefixer");
+var cleanCSS = require('gulp-clean-css');
 
 var cssUrl = "css/style.css";
 
-gulp.task("renderstyle", function(){
-	gulp.src(cssUrl)
-	.pipe(prefixer())
-	.pipe(gulp.dest("build"));
+// gulp.task("renderstyle", function(){
+// 	gulp.src(cssUrl)
+// 	.pipe(prefixer())
+// 	.pipe(gulp.dest("build"));
+// });
+
+gulp.task('format-css', function() {
+  return gulp.src(cssUrl)
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(prefixer())
+    .pipe(gulp.dest('build'));
 });
 
-
 gulp.task("watcher", function(){
-	gulp.watch(cssUrl, ["renderstyle"]);
+	gulp.watch(cssUrl, ["format-css"]);
 });
